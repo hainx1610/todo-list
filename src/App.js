2dd49dfc2198b3d14c89afcefcb785d611d18de6
Harris Nguyen <hainx1610@gmail.com>
2023-01-29 21:58:41 +0700

    refactor code into smaller components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-
+import Header from "./components/Header";
 import "./App.css";
+import TaskList from "./components/TaskList";
+import AddTaskForm from "./components/AddTaskForm";
 
 export default function App() {
   const [tasks, setTasks] = useState([
@@ -46,54 +48,19 @@ export default function App() {
 
   return (
     <div className="container">
-      <h1 className="title">
-        Todo List
-        <span>Get one item done at a time.</span>
-      </h1>
-      <ul className="task-list">
-        {tasks
-          .filter((task) => (showIncomplete ? task.status !== 1 : true))
-          .map((task) => (
-            <li key={task.id} className={task.status ? "done" : ""}>
-              <span className="label">{task.title}</span>
-              <div className="actions">
-                <input
-                  type="checkbox"
-                  className="btn-action btn-action-done"
-                  checked={Boolean(task.status)}
-                  onChange={(e) => setTaskStatus(task.id, e.target.checked)}
-                />
-                <button
-                  onClick={() => removeTask(task.id)}
-                  className="btn-action btn-action-delete"
-                >
-                  ✖
-                </button>
-              </div>
-            </li>
-          ))}
-      </ul>
-      <div className="filter-wrapper">
-        <label htmlFor="filter" className="filter-label">
-          Show incompleted tasks only
-        </label>
-        <input
-          type="checkbox"
-          id="filter"
-          checked={showIncomplete}
-          onChange={(e) => setShowIncomplete(e.target.checked)}
-        />
-      </div>
-      <form onSubmit={handleSubmit} className="form">
-        <label htmlFor="new-item">Add to the todo list</label>
-        <input
-          type="text"
-          id="new-item"
-          value={newTask}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Add item</button>
-      </form>
+      <Header title="Todo List" subTitle="Get things done" />
+      <TaskList
+        tasks={tasks}
+        showIncomplete={showIncomplete}
+        setTaskStatus={setTaskStatus}
+        removeTask={removeTask}
+        setShowIncomplete={setShowIncomplete}
+      />
+      <AddTaskForm
+        handleSubmit={handleSubmit}
+        newTask={newTask}
+        handleInputChange={handleInputChange}
+      />
     </div>
   );
 }
